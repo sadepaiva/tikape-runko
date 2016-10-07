15ce2a9ccc3146b491efda2e2deaf3bdc21fd1b1
Koski Maaria K <maariako@da5-cs-bk107-26.helsinki.fi>
2016-10-07 14:10:11 +0300

    Tallensin pari luokkaa

--- src/main/java/tikape/runko/domain/Keskustelu.java
+++ src/main/java/tikape/runko/domain/Keskustelu.java
@@ -1,14 +1,32 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package tikape.runko.domain;
 
-/**
- *
- * @author maariako
- */
+
 public class Keskustelu {
+    private String aihe;
+    private String keskustelu;
+    private Integer keskustelutunnus;
+
+    public Keskustelu(String aihe, String keskustelu, int keskustelutunnus) {
+        this.aihe = aihe;
+        this.keskustelu = keskustelu;
+        this.keskustelutunnus = keskustelutunnus;
+    }
+
+    public String getAihe() {
+        return aihe;
+    }
+
+    public void setAihe(String aihe) {
+        this.aihe = aihe;
+    }
+
+    public String getNimi() {
+        return nimi;
+    }
+
+    public void setNimi(String nimi) {
+        this.nimi = nimi;
+    }
     
 }
